63241493a5380441bfc69cb1e3f4d67011487c1f
KashGiannis34 <akashtechnion@gmail.com>
2024-01-16 00:14:42 -0500

    updated auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc8468/test/camera/sample/VisionYellowTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.ftc8468.test.camera.sample;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.ftc8468.test.camera.RobotDriveYellow;
+
+
+@Autonomous
+public class VisionYellowTest extends LinearOpMode {
+    RobotDriveYellow drive;
+    YellowPolePipelineOld pipeline;
+    private static int elementPosition = 0;
+
+    public void runOpMode() {
+        initialize();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        while (opModeIsActive() && !isStopRequested()) {
+            telemetry.addData("Status:", "running!");
+            telemetry.update();
+        }
+    }
+
+    private void initialize() {
+        drive = new RobotDriveYellow(hardwareMap);
+        pipeline = new YellowPolePipelineOld();
+        drive.startCamera(pipeline);
+        telemetry.addData("Status:", "initialize() - Robot and Camera are initialized!");
+        telemetry.update();
+    }
+
+}
